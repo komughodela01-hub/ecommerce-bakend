917620f2cd2d9f81e41ccbfd0efc2e84f09fdff5
komal ghodela <kumughodela01@gmail.com>
2026-08-20 15:55:51 +0530

    changes

--- src/config/db.js
+++ src/config/db.js
@@ -1,14 +1,15 @@
-const mongoose=require("mongoose");
+const mongoose = require("mongoose");
 
-const connectDB=async()=>{
-    try{
-        await mongoose.connect(process.env.MONGO_URL);
-        dbName: "ecommerce"
-        console.log("MongoDB Connected");
-    }catch(error){
-        console.error("Database Connection Failed:",error.message);
-        process.exit(1)
-    }
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL, {
+      dbName: "ecommerce",
+    });
+    console.log("MongoDB Connected");
+  } catch (error) {
+    console.error("Database Connection Failed:", error.message);
+    process.exit(1);
+  }
 };
 
-module.exports=connectDB;
+module.exports = connectDB;
