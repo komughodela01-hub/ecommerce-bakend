98a8d72853fb6ee7f99f1798e45c0f7b4d73d3ef
komal ghodela <kumughodela01@gmail.com>
2026-08-20 15:49:45 +0530

    db.js

--- src/config/db.js
+++ src/config/db.js
@@ -3,6 +3,7 @@ const mongoose=require("mongoose");
 const connectDB=async()=>{
     try{
         await mongoose.connect(process.env.MONGO_URL);
+        dbName: "ecommerce"
         console.log("MongoDB Connected");
     }catch(error){
         console.error("Database Connection Failed:",error.message);
